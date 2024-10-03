9472c4c0dad166e9bb1d294d33822fa52a71ce6b
David Moravec <david.moravec2@seznam.cz>
2024-10-03 17:13:02 +0200

    pouzivani vice returnu, podminka pro zobrazeni jednoho nebo druheho

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+const url = "http://api.open-notify.org/iss-now.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data["iss_position"])
+      .then((position) => {
+        setLatitude(position["latitude"])
+        setLongitude(position["longitude"])
+      })
+setLoading(false)
+
+
+  }, [])
+
+
+  if (loading) {
+    return (<h1>Načítání stránky...</h1>)
+
+  }
+
+  return (<div>
+    <h2>Zemepisna sirka</h2>
+    <p>{latitude}</p>
+    <h2>Zemepisna delka</h2>
+    <p>{longitude}</p>
+  </div>)
 }
 
-export default App;
+
+
+
+
+
+
+export default App
